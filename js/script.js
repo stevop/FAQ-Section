a249691931665d0cb5cc4468fe26164d51bdcd1a
StefanPacola <stevo.pacola@gmail.com>
2022-10-17 14:39:36 +0200

    finished accordion for mobile design

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
 const mainText = document.querySelectorAll(".main-text");
-const additionalText = document.querySelectorAll(".additional-text");
 
-//tu sa vsetkym additionalText prida class .hidden
-const hideText = additionalText.forEach(answer => {
-    answer.classList.add("hidden")
-})
-
-// tu sa na kliknutie togglne class hidden:
-const toggleText = mainText.forEach(question => {
-    question.addEventListener("click", function(){
-        question.nextElementSibling.classList.toggle("hidden")
-
-    
-    })
-})
-
-
-//teraz treba aby som ostatnym pridal hidden ak nejaky odoberiem
+mainText.forEach(question => question.addEventListener("click", () => {
+    question.parentNode.classList.toggle("active")
+}))
